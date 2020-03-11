edc2a4611be632b803477b61288eb38b12d83c72
tiennvuit <tiennvuit@gmail.com>
2020-03-11 09:59:44 +0700

    thinker

---
+++ thinkers/thinker01.py
@@ -0,0 +1,7 @@
+# Import module Thinker
+import Thinker
+
+# Initialize the window manager by Thinker.Tk() and assign it to window variable
+window = Thinker.Tk()
+
+# Rename the title of the window as you like with
