5d5c79a981ea74da6184fdc045fb7dc6cd3a1170
tiennv <tiennvuit@gmail.com>
2020-03-25 14:55:15 +0700

    remove don need files

--- tkinter/tkinter01.py
+++
@@ -1,9 +0,0 @@
-# Import module Tkinter
-from Tkinter import * 
-
-# Initialize the window manager by Thinker.Tk() and assign it to window variable
-window = Tk()
-
-# Rename the title of the window as you like with
-window.mainloop()
-# Start processing 
